2e5533a7e2b2f82df7eb684ecf933171060d54b1
Jorge Darderes <JDARDERES@hotmail.com>
2023-11-16 11:56:31 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:animated_button/animated_button.dart';
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+       
+        body: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/image/fondo_madera.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          
+          child: const Marimba()),
+      ),
+    );
+  }
+}
+
+class Marimba extends StatelessWidget {
+  const Marimba({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeRight]);
+
+  return Column(
+      children: [
+        const SizedBox(height: 24.0), // Espaciado superior
+        Expanded(
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
+      
+        children: [
+          MarimbaKey(note: 'a',soundPath: 'sounds/marimba1.mp3',buttonColor: Colors.red,),
+          MarimbaKey(note: 'b',soundPath: 'sounds/marimba2.mp3',buttonColor: Colors.blue,),
+          MarimbaKey(note: 'C',soundPath: 'sounds/marimba3.mp3',buttonColor: Colors.brown,),
+          MarimbaKey(note: 'D',soundPath: 'sounds/marimba4.mp3',buttonColor: Colors.green,),
+          MarimbaKey(note: 'e',soundPath: 'sounds/marimba5.mp3',buttonColor: Colors.yellow,),
+          MarimbaKey(note: 'f',soundPath: 'sounds/marimba6.mp3',buttonColor: Colors.purple,),
+          MarimbaKey(note: 'g',soundPath: 'sounds/marimba7.mp3',buttonColor: Colors.orange,),
+          MarimbaKey(note: 'h',soundPath: 'sounds/marimba8.mp3',buttonColor: Colors.pink,),
+       
+      
+          // Agrega más teclas de la marimba según sea necesario
+     ],
+          ),
+        ),
+        const SizedBox(height: 16.0), // Espaciado inferior
+      ],
+    );
+  }
+}
+
+
+class MarimbaKey extends StatelessWidget {
+  final String note;
+  final String soundPath ;
+   final Color buttonColor; // Ajusta según tus archivos de sonido
+
+  MarimbaKey({super.key, required this.note,required this.soundPath,required this.buttonColor});
+
+  final AudioPlayer player = AudioPlayer();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Expanded(
+          child: AnimatedButton(
+            shadowDegree: ShadowDegree.dark,
+            color: buttonColor,
+            onPressed: () async {
+               player.setReleaseMode(ReleaseMode.release);
+              await player.play(AssetSource(soundPath));
+            },
+            //    style: ButtonStyle(
+            //   backgroundColor: MaterialStateProperty.all<Color>(buttonColor),
+             
+            // ),
+            child: Center(
+              child: Text(
+                note,
+                style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
